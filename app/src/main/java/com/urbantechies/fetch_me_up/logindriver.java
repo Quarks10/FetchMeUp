8452622e280180b27dde14a4ca7d9c2c5dc2396f
Atiq <atiqah09@gmail.com>
2019-07-14 16:19:18 +0800

    startup page initiate

--- app/src/main/java/com/urbantechies/fetch_me_up/logindriver.java
+++ app/src/main/java/com/urbantechies/fetch_me_up/logindriver.java
@@ -3,11 +3,11 @@ package com.urbantechies.fetch_me_up;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class logindriver extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_logindriver);
     }
 }
---
+++ app/src/main/java/com/urbantechies/fetch_me_up/startup.java
@@ -0,0 +1,13 @@
+package com.urbantechies.fetch_me_up;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class startup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startup);
+    }
+}

0b28a8e5a177aa0b1d2117d9cfecc1d3416f2fcd
Ahmad Syahir <syahirzahiri@gmail.com>
2019-07-15 15:50:01 +0800

    added bottom navigation

--- app/src/main/java/com/urbantechies/fetch_me_up/HomePage.java
+++ app/src/main/java/com/urbantechies/fetch_me_up/HomePage.java
@@ -1,7 +1,9 @@
 package com.urbantechies.fetch_me_up;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +15,8 @@ import android.view.MenuItem;
 public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
 
 
     @Override
@@ -24,8 +28,11 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -33,25 +40,55 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
         toggle.syncState();
 
         if (savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                    new InboxFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_inbox);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment,
+                    new HomeFragment()).commit();
+            bottomNavigationView.setSelectedItemId(R.id.nav_home);
         }
+
+
+
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_request:
+                            selectedFragment = new PassengerRequestFragment();
+                            break;
+                        case R.id.nav_custom_ride:
+                            selectedFragment = new CustomRideFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()){
             case R.id.nav_inbox:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,
                         new InboxFragment()).commit();
                 break;
             case R.id.nav_trip:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,
                         new MyTripFragment()).commit();
                 break;
             case R.id.nav_wallet:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment,
                         new WalletFragment()).commit();
                 break;
         }

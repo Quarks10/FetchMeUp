f3cffff6167c5dc1400025b4545544d4e851169a
Ahmad Syahir <syahirzahiri@gmail.com>
2019-07-16 10:59:33 +0800

    migrate to androidx IMPORTANT to pull first

--- app/src/main/java/com/urbantechies/fetch_me_up/logindriver.java
+++ app/src/main/java/com/urbantechies/fetch_me_up/logindriver.java
@@ -1,6 +1,6 @@
 package com.urbantechies.fetch_me_up;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class logindriver extends AppCompatActivity {
